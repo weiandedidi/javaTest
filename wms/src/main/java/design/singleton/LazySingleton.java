b5b34ea27a97bdab2931767e3345cb997fab016b
weiandedidi@163.com <weiandedidi@163.com>
2017-10-18 11:58:15 +0800

    fix

--- wms/src/main/java/design/singleton/LazySingleton.java
+++ wms/src/main/java/design/singleton/LazySingleton.java
@@ -12,7 +12,6 @@ public class LazySingleton {
 
     private LazySingleton() {
     }
-/*
     //锁方法 ==锁类
     public synchronized LazySingleton getInstance() {
         if (null == instance) {
@@ -20,7 +19,6 @@ public class LazySingleton {
         }
         return instance;
     }
-    */
 
 /*    //锁对象
     public LazySingleton getInstance() {
@@ -32,22 +30,6 @@ public class LazySingleton {
         return instance;
     }*/
 
-    // 两层锁
-    // 需要注意的是，如果使用双重检查锁定来实现懒汉式单例类，需要在静态成员变量instance之前增加修饰符volatile，
-    // 被volatile修饰的成员变量可以确保多个线程都能够正确处理，且该代码只能在JDK 1.5及以上版本中才能正确执行
-    public synchronized LazySingleton getInstance() {
-        //第一重判断
-        if (instance == null) {
-            //锁定代码块
-            synchronized (LazySingleton.class) {
-                //第二重判断
-                if (instance == null) {
-                    instance = new LazySingleton(); //创建单例实例
-                }
-            }
-        }
-        return instance;
-    }
 
 
 }
---
+++ wms/src/main/java/design/singleton/LazySingletonDoubleLock.java
@@ -0,0 +1,34 @@
+package design.singleton;
+
+/**
+ * 两层锁
+ * 需要注意的是，如果使用双重检查锁定来实现懒汉式单例类，需要在静态成员变量instance之前增加修饰符volatile，
+ * 被volatile修饰的成员变量可以确保多个线程都能够正确处理，且该代码只能在JDK 1.5及以上版本中才能正确执行
+ * Created with IntelliJ IDEA.
+ * User: qidima
+ * Date: 2017/10/18
+ * Time: 11:43
+ */
+public class LazySingletonDoubleLock {
+    private volatile static LazySingletonDoubleLock instance = null;
+
+    //私有的方法
+    private LazySingletonDoubleLock() {
+    }
+
+    //双重内部锁
+    public synchronized LazySingletonDoubleLock getInstance() {
+        //第一重判断
+        if (instance == null) {
+            //锁定代码块
+            synchronized (LazySingletonDoubleLock.class) {
+                //第二重判断
+                if (instance == null) {
+                    instance = new LazySingletonDoubleLock(); //创建单例实例
+                }
+            }
+        }
+        return instance;
+    }
+
+}

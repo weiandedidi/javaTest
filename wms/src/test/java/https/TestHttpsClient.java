75d0a740817320bd5ec7e73b330ee0268942f832
马启迪 <qidima@maqidideMacBook-Pro.local>
2018-01-13 21:14:26 +0800

    fix

--- wms/src/test/java/https/TestHttpsClient.java
+++ wms/src/test/java/https/TestHttpsClient.java
@@ -25,6 +25,13 @@ public class TestHttpsClient {
     private String charset = "utf-8";
     private String sysId = "106605";
 
+    /**
+     * 测试为知笔记的https的json登陆
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchProviderException
+     * @throws KeyManagementException
+     */
     @Test
     public void testHttpsPost() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, KeyManagementException {
         Map<String, String> headerMap = new HashMap<String, String>();
@@ -35,13 +42,43 @@ public class TestHttpsClient {
         headerMap.put("Accept","*/*");
         Map<String, String> params = new HashMap<String, String>();
         url = "https://note.wiz.cn/as/user/login?clientType=web&clientVersion=3.0.0&apiVersion=10&lang=zh-cn";
-        params.put("userId", "weiandedidi@163.com");
-        params.put("password", "maqidi4915338");
+        params.put("userId", "");
+        params.put("password", "");
 
         String data = JSON.toJSONString(params);
         String result = HttpsUtils.doHttpsPost(url,data,headerMap);
         System.out.println(result);
 
+
+    }
+
+    /**
+     * 测试北邮人https的form登陆
+     * @throws IOException
+     * @throws NoSuchAlgorithmException
+     * @throws NoSuchProviderException
+     * @throws KeyManagementException
+     */
+    @Test
+    public void testHttpsPostV2() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, KeyManagementException {
+        Map<String, String> headerMap = new HashMap<String, String>();
+        headerMap.put("accept", "*/*");
+        headerMap.put("connection", "Keep-Alive");
+        headerMap.put("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36");
+        headerMap.put("content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+        headerMap.put("origin","https://bbs.byr.cn");
+        headerMap.put("referer","https://bbs.byr.cn/");
+        headerMap.put("Accept","*/*");
+        headerMap.put("x-requested-with","XMLHttpRequest");
+
+        Map<String, String> params = new HashMap<String, String>();
+        url = "https://bbs.byr.cn/user/ajax_login.json";
+        params.put("id", "weiandedidi");
+        params.put("passwd", "maqidi4915338");
+        String data = HttpsUtils.contactGetString(params);
+
+        String result = HttpsUtils.doHttpsPost(url,data,headerMap);
+        System.out.println(result);
 
     }
 

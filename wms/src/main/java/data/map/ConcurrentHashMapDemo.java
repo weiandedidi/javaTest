29b55f18ce23fcdc8749e5795a128a99ffc6512b
weiandedidi <weiandedidi@163.com>
2018-05-14 18:01:58 +0800

    学习线程安全的map

---
+++ wms/src/main/java/data/map/ConcurrentHashMapDemo.java
@@ -0,0 +1,19 @@
+package data.map;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 学习concurrentHashMap
+ *
+ * @author: qidima
+ * @date: 2018/5/14
+ * Time: 17:55
+ */
+public class ConcurrentHashMapDemo {
+    /**
+     * 参数map的开发
+     */
+    ConcurrentHashMap<Long, Object> configMap = new ConcurrentHashMap<Long, Object>();
+
+
+}

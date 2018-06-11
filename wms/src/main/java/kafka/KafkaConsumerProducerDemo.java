24dd8480a620e93b7c2951955e83431d7e6e49f8
weiandedidi <weiandedidi@163.com>
2018-06-11 11:57:02 +0800

    kafka 哭护短

---
+++ wms/src/main/java/kafka/KafkaConsumerProducerDemo.java
@@ -0,0 +1,18 @@
+package kafka;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: qidima
+ * @date: 2018/6/11
+ * Time: 11:46
+ */
+public class KafkaConsumerProducerDemo {
+    public static void main(String[] args) {
+        KafkaProducer producerThread = new KafkaProducer(KafkaProperties.topic);
+        producerThread.start();
+        KafkaConsumer consumerThread = new KafkaConsumer(KafkaProperties.topic);
+        consumerThread.start();
+    }
+}

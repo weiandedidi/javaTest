f08f24c0b93fc3ae488bba3c5e7effd4b3b1f8ca
马启迪 <maqidi@meituan.com>
2018-12-14 22:01:36 +0800

    lamada

--- javacore/src/main/java/lamada/CollectionDemo.java
+++ javacore/src/main/java/lamada/CollectionDemo.java
@@ -1,16 +1,21 @@
 package lamada;
 
 import com.google.common.collect.Lists;
+import lombok.Data;
 
 import java.util.List;
 
 /**
  * 集合方法
+ * :: 直接访问Class的静态方法 关键字来访问类的构造方法，对象方法，静态方法。
+ * <p>
  * User: qidi
  * Date: 2018/12/14
  * Time: 下午9:23
  */
+@Data
 public class CollectionDemo {
+    private String name;
 
     private static void print(Integer number) {
         System.out.println(number);
@@ -38,10 +43,14 @@ public class CollectionDemo {
         //打印的方法  双:: 直接访问Class的静态方法
         numbers.forEach(CollectionDemo::print);
 
+
     }
 
     private interface CovertStrToInteger {
         Integer covertStrToInteger(String number);
+    }
+
+    public void hi() {
     }
 
 

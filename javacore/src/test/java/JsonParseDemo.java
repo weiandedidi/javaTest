391cbd73420d110094b2edd229791ffc7127bcb9
马启迪 <maqidi@meituan.com>
2019-04-18 15:33:23 +0800

    测试string转为string[]

--- javacore/src/test/java/JsonParseDemo.java
+++ javacore/src/test/java/JsonParseDemo.java
@@ -1,7 +1,9 @@
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author qidi
@@ -9,17 +11,24 @@ import java.math.BigDecimal;
  */
 public class JsonParseDemo {
     public static void main(String[] args) {
-        String json = "{\"name\":\"ma\",\"age\":\"5.58\"}";
+
+        String json = "{\"name\":\"ma\",\"age\":\"45.6\",\"data\":[\"a\",\"b\",\"c\"]}";
 
         Aoo aoo = JSON.parseObject(json, Aoo.class);
-        System.out.println(aoo.getAge());
+        System.out.println(aoo.getData());
+
+
+        List<String> data = JSONArray.parseArray(aoo.getData(), String.class);
+        System.out.println(data.get(0));
 
     }
+
 
     @Data
     static class Aoo {
         private String name;
         private BigDecimal age;
+        private String data;
     }
 
 }

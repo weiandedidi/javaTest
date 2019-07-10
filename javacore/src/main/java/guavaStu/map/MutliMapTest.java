f113e25665e1baf518d0c791546b3f2b27655c20
马启迪 <maqidi@meituan.com>
2019-07-10 15:14:14 +0800

    多功能map

--- javacore/src/main/java/guavaStu/map/MutliMapTest.java
+++ javacore/src/main/java/guavaStu/map/MutliMapTest.java
@@ -3,11 +3,16 @@ package guavaStu.map;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
 
 import java.util.Collection;
+import java.util.Set;
 
 /**
  * Guava 之 Multimap
+ * 多映射的多功能map
+ * 可实现一个k对应多个v
+ * <p>
  * User: qidi
  * Date: 2018/12/14
  * Time: 下午8:24
@@ -15,39 +20,40 @@ import java.util.Collection;
 public class MutliMapTest {
 
     public static void main(String[] args) {
-        Multimap<String, String> myMultimap = ArrayListMultimap.create();
+        Multimap<String, String> multimap = ArrayListMultimap.create();
 
         // Adding some key/value
-        myMultimap.put("Fruits", "Bannana");
-        myMultimap.put("Fruits", "Apple");
-        myMultimap.put("Fruits", "Pear");
-        myMultimap.put("Fruits", "Pear");
-        myMultimap.put("Vegetables", "Carrot");
+        multimap.put("Fruits", "Bannana");
+        multimap.put("Fruits", "Apple");
+        multimap.put("Fruits", "Pear");
+        multimap.put("Fruits", "Pear");
+        multimap.put("Vegetables", "Carrot");
 
         // Getting the size
-        int size = myMultimap.size();
-        System.out.println(size); // 5
+        int size = multimap.size();
+        System.out.println(size);
 
         // Getting values
-        Collection<String> fruits = myMultimap.get("Fruits");
+        Collection<String> fruits = multimap.get("Fruits");
         System.out.println(fruits); //  [Bannana, Apple, Pear, Pear]
-        System.out.println(ImmutableSet.copyOf(fruits));// [Bannana, Apple, Pear]
+        // [Bannana, Apple, Pear]
+        System.out.println(ImmutableSet.copyOf(fruits));
 
-        Collection<String> vegetables = myMultimap.get("Vegetables");
+        Collection<String> vegetables = multimap.get("Vegetables");
         System.out.println(vegetables); // [Carrot]
 
         // Iterating over entire Mutlimap
-        for (String value : myMultimap.values()) {
+        for (String value : multimap.values()) {
             System.out.println(value);
         }
 
         // Removing a single value
-        myMultimap.remove("Fruits", "Pear");
-        System.out.println(myMultimap.get("Fruits")); // [Bannana, Apple, Pear]
+        multimap.remove("Fruits", "Pear");
+        System.out.println(multimap.get("Fruits")); // [Bannana, Apple, Pear]
 
         // Remove all values for a key
-        myMultimap.removeAll("Fruits");
-        System.out.println(myMultimap.get("Fruits")); // [] (Empty Collection!)
+        multimap.removeAll("Fruits");
+        System.out.println(multimap.get("Fruits")); // [] (Empty Collection!)
 
 
     }

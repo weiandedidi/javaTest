9c1332a6bf4f4c22adad881ac5b9504416f9b76a
马启迪 <maqidi@meituan.com>
2020-01-17 19:24:02 +0800

    修改所有方法

--- javacore/src/main/java/algorithm/leetcode/TopKFrequent.java
+++ javacore/src/main/java/algorithm/leetcode/TopKFrequent.java
@@ -1,8 +1,41 @@
 package algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+
 /**
  * @author qidi
  * @date 2020-01-16 17:14
  */
 public class TopKFrequent {
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        //按照出现次数分成小堆
+        HashMap<Integer, Integer> num2CountMap = new HashMap();
+        for (int n : nums) {
+            num2CountMap.put(n, num2CountMap.getOrDefault(n, 0) + 1);
+        }
+
+        //构建堆
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>((n1, n2) -> num2CountMap.get(n1) - num2CountMap.get(n2));
+
+        //堆中找k个元素
+        for (int n : num2CountMap.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+        //输出结果
+        List<Integer> result = new ArrayList<>();
+        while (!heap.isEmpty()) {
+            result.add(heap.poll());
+        }
+
+        return result;
+    }
+
 }
--- javacore/src/main/java/thread/semaphore/SemaphoreDemo.java
+++ javacore/src/main/java/thread/semaphore/SemaphoreDemo.java
@@ -1,8 +1,49 @@
 package thread.semaphore;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
 /**
+ * 信号箱，一个萝卜一个坑
+ *
  * @author qidi
  * @date 2020-01-17 19:03
  */
 public class SemaphoreDemo {
+    public static void main(String[] args) {
+        //建立流量池
+        ExecutorService service = new ThreadPoolExecutor(10, 15, 0, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>(1024));
+//        ExecutorService service = Executors.newCachedThreadPool();
+
+        //3个进入量
+        final Semaphore semaphore = new Semaphore(3);
+        System.out.println("初始化：当前有" + (3 - semaphore.availablePermits() + "个并发"));
+
+        //创建10个任务，上面的缓存线程池就会创建10个对应的线程去执行
+        for (int index = 0; index < 10; index++) {
+            final int NO = index;  //记录第几个任务
+            Runnable run = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        //获取进入权限
+                        semaphore.acquire();
+                        System.out.println(Thread.currentThread().getName() + "获取许可" + "(" + NO + ")，" + "剩余：" + semaphore.availablePermits());
+                        Thread.sleep(1000);
+                        // 访问完后记得释放 ，否则在控制台只能打印3条记录，之后线程一直阻塞
+                        semaphore.release();  //释放许可
+                        System.out.println(Thread.currentThread().getName() + "释放许可" + "(" + NO + ")，" + "剩余：" + semaphore.availablePermits());
+
+                    } catch (InterruptedException e) {
+                        System.out.println("获取失败");
+                        e.printStackTrace();
+                    }
+                }
+            };
+            service.submit(run);
+        }
+        //关闭
+        service.shutdown();
+    }
 }
